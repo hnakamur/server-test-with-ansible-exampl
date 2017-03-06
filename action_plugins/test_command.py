a3862cacad75569a11dfb5dc9c8cebcecd3c2d2c
Hiroaki Nakamura <hnakamur@gmail.com>
2017-03-06 14:36:48 +0900

    Add rc, stdout, stderr to test_command result

--- action_plugins/test_command.py
+++ action_plugins/test_command.py
@@ -41,14 +41,22 @@ class ActionModule(ActionBase):
             task_vars = dict()
 
         result = super(ActionModule, self).run(tmp, task_vars)
+        if 'failed' not in result:
+            result['failed'] = False
 
         cmd = self._task.args.get('cmd', None)
         want_stdout = self._task.args.get('want_stdout', None)
+        if 'want_stdout' in self._task.args:
+            result['want_stdout'] = want_stdout
 
         cmd_result = self._low_level_execute_command(cmd=cmd)
+        result['cmd'] = cmd
+        result['rc'] = cmd_result['rc']
+        result['stdout'] = cmd_result['stdout']
+        result['stderr'] = cmd_result['stderr']
+
         got_stdout = cmd_result["stdout"].rstrip("\n")
         if want_stdout is not None and got_stdout != want_stdout:
-            self._display.display("cmd: {0}".format(cmd), color=C.COLOR_OK)
             diff = {
                 "before": want_stdout + "\n",
                 "after": got_stdout + "\n",
@@ -57,153 +65,5 @@ class ActionModule(ActionBase):
             }
             result['diff'] = diff
             result['changed'] = True
-
-#        source = self._task.args.get('src', None)
-#        dest   = self._task.args.get('dest', None)
-#        force  = boolean(self._task.args.get('force', True))
-#        state  = self._task.args.get('state', None)
-#
-#        if state is not None:
-#            result['failed'] = True
-#            result['msg'] = "'state' cannot be specified on a template"
-#        elif source is None or dest is None:
-#            result['failed'] = True
-#            result['msg'] = "src and dest are required"
-#        else:
-#            try:
-#                source = self._find_needle('templates', source)
-#            except AnsibleError as e:
-#                result['failed'] = True
-#                result['msg'] = to_native(e)
-#
-#        if 'failed' in result:
-#            return result
-#
-#        # Expand any user home dir specification
-#        dest = self._remote_expand_user(dest)
-#
-#        directory_prepended = False
-#        if dest.endswith(os.sep):
-#            directory_prepended = True
-#            base = os.path.basename(source)
-#            dest = os.path.join(dest, base)
-#
-#        # template the source data locally & get ready to transfer
-#        b_source = to_bytes(source)
-#        try:
-#            with open(b_source, 'r') as f:
-#                template_data = to_text(f.read())
-#
-#            try:
-#                template_uid = pwd.getpwuid(os.stat(b_source).st_uid).pw_name
-#            except:
-#                template_uid = os.stat(b_source).st_uid
-#
-#            temp_vars = task_vars.copy()
-#            temp_vars['template_host']     = os.uname()[1]
-#            temp_vars['template_path']     = source
-#            temp_vars['template_mtime']    = datetime.datetime.fromtimestamp(os.path.getmtime(b_source))
-#            temp_vars['template_uid']      = template_uid
-#            temp_vars['template_fullpath'] = os.path.abspath(source)
-#            temp_vars['template_run_date'] = datetime.datetime.now()
-#
-#            managed_default = C.DEFAULT_MANAGED_STR
-#            managed_str = managed_default.format(
-#                host = temp_vars['template_host'],
-#                uid  = temp_vars['template_uid'],
-#                file = to_bytes(temp_vars['template_path'])
-#            )
-#            temp_vars['ansible_managed'] = time.strftime(
-#                managed_str,
-#                time.localtime(os.path.getmtime(b_source))
-#            )
-#
-#
-#            searchpath = []
-#            # set jinja2 internal search path for includes
-#            if 'ansible_search_path' in task_vars:
-#                searchpath = task_vars['ansible_search_path']
-#                # our search paths aren't actually the proper ones for jinja includes.
-#
-#            searchpath.extend([self._loader._basedir, os.path.dirname(source)])
-#
-#            # We want to search into the 'templates' subdir of each search path in
-#            # addition to our original search paths.
-#            newsearchpath = []
-#            for p in searchpath:
-#                newsearchpath.append(os.path.join(p, 'templates'))
-#                newsearchpath.append(p)
-#            searchpath = newsearchpath
-#
-#            self._templar.environment.loader.searchpath = searchpath
-#
-#            old_vars = self._templar._available_variables
-#            self._templar.set_available_variables(temp_vars)
-#            resultant = self._templar.do_template(template_data, preserve_trailing_newlines=True, escape_backslashes=False)
-#            self._templar.set_available_variables(old_vars)
-#        except Exception as e:
-#            result['failed'] = True
-#            result['msg'] = type(e).__name__ + ": " + str(e)
-#            return result
-#
-#        remote_user = task_vars.get('ansible_ssh_user') or self._play_context.remote_user
-#        if not tmp:
-#            tmp = self._make_tmp_path(remote_user)
-#            self._cleanup_remote_tmp = True
-#
-#        local_checksum = checksum_s(resultant)
-#        remote_checksum = self.get_checksum(dest, task_vars, not directory_prepended, source=source, tmp=tmp)
-#        if isinstance(remote_checksum, dict):
-#            # Error from remote_checksum is a dict.  Valid return is a str
-#            result.update(remote_checksum)
-#            return result
-#
-#        diff = {}
-#        new_module_args = self._task.args.copy()
-#
-#        if (remote_checksum == '1') or (force and local_checksum != remote_checksum):
-#
-#            result['changed'] = True
-#            # if showing diffs, we need to get the remote value
-#            if self._play_context.diff:
-#                diff = self._get_diff_data(dest, resultant, task_vars, source_file=False)
-#
-#            if not self._play_context.check_mode:  # do actual work through copy
-#                xfered = self._transfer_data(self._connection._shell.join_path(tmp, 'source'), resultant)
-#
-#                # fix file permissions when the copy is done as a different user
-#                self._fixup_perms2((tmp, xfered), remote_user)
-#
-#                # run the copy module
-#                new_module_args.update(
-#                   dict(
-#                       src=xfered,
-#                       dest=dest,
-#                       original_basename=os.path.basename(source),
-#                       follow=True,
-#                   ),
-#                )
-#                result.update(self._execute_module(module_name='copy', module_args=new_module_args, task_vars=task_vars, tmp=tmp, delete_remote_tmp=False))
-#
-#            if result.get('changed', False) and self._play_context.diff:
-#                result['diff'] = diff
-#
-#        else:
-#            # when running the file module based on the template data, we do
-#            # not want the source filename (the name of the template) to be used,
-#            # since this would mess up links, so we clear the src param and tell
-#            # the module to follow links.  When doing that, we have to set
-#            # original_basename to the template just in case the dest is
-#            # a directory.
-#            new_module_args.update(
-#                dict(
-#                    src=None,
-#                    original_basename=os.path.basename(source),
-#                    follow=True,
-#                ),
-#            )
-#            result.update(self._execute_module(module_name='file', module_args=new_module_args, task_vars=task_vars, tmp=tmp, delete_remote_tmp=False))
-#
-#        self._remove_tmp_path(tmp)
 
         return result
